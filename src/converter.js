07fc328c4bd94326a389488e7e5fddbfcfe3e511
Jakub <jakub.raban@gmail.com>
2021-02-20 18:06:38 +0100

    Implement converter

--- src/converter.js
+++ src/converter.js
@@ -1,13 +1,44 @@
-const convert = (wielkość, oldUnitSpec, newUnitSpec) => {
-    oldUnitSpec = {
-        unit: 'MeV',
-        particle: 'proton',
-    };
-    newUnitSpec = {
-        unit: 'MeV/u',
-        particle: 'proton'
+/*
+
+spec:
+{
+    unit,
+    particle,
+}
+
+ */
+
+import {units} from "./units";
+
+const MULTIPLICATION_IDENTITY = 1;
+
+export const convertBetweenUnits = (quantity, oldSpec, newSpec) => {
+    const { unit: oldUnit, particle: oldParticle } = oldSpec;
+    const { unit: newUnit, particle: newParticle } = newSpec;
+    if (oldUnit !== newUnit) {
+        let conversion = 1;
+        const unitValues = units[quantity];
+        const [oldUnitNominator, oldUnitDenominator] = oldUnit.split('/');
+        const [newUnitNominator, newUnitDenominator] = newUnit.split('/');
+        if (oldUnitNominator !== newUnitNominator)
+            conversion *= unitValues[newUnitNominator] / unitValues[oldUnitNominator];
+        if (!oldUnitDenominator && newUnitDenominator === 'u')
+            conversion /= particleMasses[newParticle];
+        if (oldUnitDenominator === 'u' && !newUnitDenominator)
+            conversion *= particleMasses[newParticle];
+        return conversion;
     }
-    if (newUnitSpec.unit === 'xd') {
-        return proton.mass
+    if (oldParticle !== newParticle) {
+        if (!oldUnit.endsWith('/u')) return MULTIPLICATION_IDENTITY;
+        return particleMasses[oldParticle] / particleMasses[newParticle];
     }
+    return MULTIPLICATION_IDENTITY;
+}
+
+//BEFORE: energy[KeV/u] = 5000, particle = Alpha
+//AFTER: particle = Beta
+
+const particleMasses = {
+    '1001': 1,
+    '2004': 4,
 }
